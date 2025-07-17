ea39484ed4a1729ef17301e434b3ac997d88963e
MorseWayne <2539720515@qq.com>
2025-07-17 08:08:56 +0000

    1. 前端首页加载功能适配 2. 后端增加日志记录功能

--- forum/backend/forum_server/src/main.rs
+++ forum/backend/forum_server/src/main.rs
@@ -1,14 +1,23 @@
-use actix_web::{web, App, HttpServer, Responder};
-
-async fn index() -> impl Responder {
-    "Hello, World!"
-}
+use actix_web::{App, HttpServer, middleware::Logger};
+use actix_files::Files;
+use log::info;
+use log4rs;  // 导入log4rs
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()>
+{
+    // 从配置文件初始化log4rs
+    log4rs::init_file("log4rs.yml", Default::default())
+        .expect("Failed to initialize log4rs");
+
+    info!("Server starting");
+
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(index))
+            .wrap(Logger::default())  // HTTP请求日志中间件
+            .service(Files::new("/", "/home/wayne/source/practice/forum/frontend")
+                .index_file("index.html")
+                .prefer_utf8(true))
     })
     .bind("0.0.0.0:8080")?
     .run()
